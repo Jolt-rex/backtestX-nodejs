14cdc089735d48c7da8f89722289781ead78bd8a
Lee <leepotter010@gmail.com>
2022-11-13 20:30:30 +1000

    Added comments and pseudocode for binanceAPI for multiple webSocket connection implementation

--- services/binanceAPI.js
+++ services/binanceAPI.js
@@ -3,8 +3,14 @@ const Binance = require('node-binance-api');
 const config = require('config');
 const winston = require('winston');
 
+const MAX_PAIRS_PER_SOCKET = 200;
 const websocketURL = 'wss://stream.binance.com:9443/stream?streams=';
 const webSockets = [];
+// webSockets is a list of objects as per below
+// {
+//    connectionUrl: 'wss://stream.binance.com:9443/stream?streams=ethbtc@kline1m/linkusdt@kline1m',
+//    count: 0    
+// }
 
 
 module.exports.binance = new Binance().options({
@@ -31,4 +37,14 @@ function registerSocket(ws, callback) {
     // if this is a closed kline, execute the callback with closed candle data
     if (data.k.x) callback(data.k);
   });
+}
+
+// takes in trading pair string in form of 'BTCUSDT'
+// adds trading pair to webSockets list of object
+// each webSockets object can have up to 200 pairs
+function addWebSocketConnection(tradingPair) {
+  // if webSockets size == 0 or last element.count == 200
+    // create new webSockets object
+  
+  // add pair to last webSocket connection
 }
